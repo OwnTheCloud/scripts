1f8b70f0305501d685c1fb36279105f8ebf338ca
OwnTheCloud <owner@ownthe.cloud>
2019-10-20 09:18:37 -0700

    update ip

--- StartServer.sh
+++ StartServer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script will start the hugo server on port 80
-sudo hugo server --bind=0.0.0.0 --baseUrl=http://192.168.1.130/ --port=80 --appendPort=false
-echo "The Hugo Server is started, surf to 192.168.1.130!"
+sudo hugo server --bind=0.0.0.0 --baseUrl=http://192.168.1.184/ --port=80 --appendPort=false -s /home/pi/GitHub/ownthecloud/
+echo "The Hugo Server is started, surf to 192.168.1.184!"
